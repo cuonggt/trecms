7377d8ba204a17ced7c4d27cf874edfe6bbafa14
Cuong Giang <thaicuong.giang@gmail.com>
2023-11-19 10:23:37 +0700

    fix tiptap toolbar buttons to not autosubmit form

--- resources/views/components/tiptap-editor.blade.php
+++ resources/views/components/tiptap-editor.blade.php
@@ -7,37 +7,37 @@
 >
     <template x-if="isLoaded()">
         <div class="flex space-x-4 items-center dark:text-neutral-100 fill-current py-2">
-            <button @click="toggleBold()" :class="{ 'bg-gray-200 rounded': isActive('bold', updatedAt) }" class="p-1">
+            <button type="button" @click.prevent="toggleBold()" :class="{ 'bg-gray-200 rounded': isActive('bold', updatedAt) }" class="p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bold"><path d="M14 12a4 4 0 0 0 0-8H6v8"/><path d="M15 20a4 4 0 0 0 0-8H6v8Z"/></svg>
             </button>
-            <button @click="toggleItalic()" :class="{ 'bg-gray-200 rounded': isActive('italic', updatedAt) }" class="p-1">
+            <button type="button" @click.prevent="toggleItalic()" :class="{ 'bg-gray-200 rounded': isActive('italic', updatedAt) }" class="p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-italic"><line x1="19" x2="10" y1="4" y2="4"/><line x1="14" x2="5" y1="20" y2="20"/><line x1="15" x2="9" y1="4" y2="20"/></svg>
             </button>
-            <button @click="toggleStrike()" :class="{ 'bg-gray-200 rounded': isActive('strike', updatedAt) }" class="p-1">
+            <button type="button" @click.prevent="toggleStrike()" :class="{ 'bg-gray-200 rounded': isActive('strike', updatedAt) }" class="p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-strikethrough"><path d="M16 4H9a3 3 0 0 0-2.83 4"/><path d="M14 12a4 4 0 0 1 0 8H6"/><line x1="4" x2="20" y1="12" y2="12"/></svg>
             </button>
-            <button @click="setLink()" :class="{ 'bg-gray-200 rounded': isActive('link', updatedAt) }" class="p-1">
+            <button type="button" @click="setLink()" :class="{ 'bg-gray-200 rounded': isActive('link', updatedAt) }" class="p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg>
             </button>
-            <button @click="toggleHeading({ level: 1 })" :class="{ 'bg-gray-200 rounded': isActive('heading', { level: 1 }, updatedAt) }" class="p-1">
+            <button type="button" @click.prevent="toggleHeading({ level: 1 })" :class="{ 'bg-gray-200 rounded': isActive('heading', { level: 1 }, updatedAt) }" class="p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heading-1"><path d="M4 12h8"/><path d="M4 18V6"/><path d="M12 18V6"/><path d="m17 12 3-2v8"/></svg>
             </button>
-            <button @click="toggleHeading({ level: 2 })" :class="{ 'bg-gray-200 rounded': isActive('heading', { level: 2 }, updatedAt) }" class="p-1">
+            <button type="button" @click.prevent="toggleHeading({ level: 2 })" :class="{ 'bg-gray-200 rounded': isActive('heading', { level: 2 }, updatedAt) }" class="p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heading-2"><path d="M4 12h8"/><path d="M4 18V6"/><path d="M12 18V6"/><path d="M21 18h-4c0-4 4-3 4-6 0-1.5-2-2.5-4-1"/></svg>
             </button>
-            <button @click="toggleBlockquote()" :class="{ 'bg-gray-200 rounded': isActive('blockquote', updatedAt) }" class="p-1">
+            <button type="button" @click.prevent="toggleBlockquote()" :class="{ 'bg-gray-200 rounded': isActive('blockquote', updatedAt) }" class="p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-quote"><path d="M3 21c3 0 7-1 7-8V5c0-1.25-.756-2.017-2-2H4c-1.25 0-2 .75-2 1.972V11c0 1.25.75 2 2 2 1 0 1 0 1 1v1c0 1-1 2-2 2s-1 .008-1 1.031V20c0 1 0 1 1 1z"/><path d="M15 21c3 0 7-1 7-8V5c0-1.25-.757-2.017-2-2h-4c-1.25 0-2 .75-2 1.972V11c0 1.25.75 2 2 2h.75c0 2.25.25 4-2.75 4v3c0 1 0 1 1 1z"/></svg>
             </button>
-            <button @click="toggleCodeBlock()" :class="{ 'bg-gray-200 rounded': isActive('codeBlock', updatedAt) }" class="p-1">
+            <button type="button" @click.prevent="toggleCodeBlock()" :class="{ 'bg-gray-200 rounded': isActive('codeBlock', updatedAt) }" class="p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-code"><polyline points="16 18 22 12 16 6"/><polyline points="8 6 2 12 8 18"/></svg>
             </button>
-            <button @click="toggleBulletList()" :class="{ 'bg-gray-200 rounded': isActive('bulletList', updatedAt) }" class="p-1">
+            <button type="button" @click.prevent="toggleBulletList()" :class="{ 'bg-gray-200 rounded': isActive('bulletList', updatedAt) }" class="p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-list"><line x1="8" x2="21" y1="6" y2="6"/><line x1="8" x2="21" y1="12" y2="12"/><line x1="8" x2="21" y1="18" y2="18"/><line x1="3" x2="3.01" y1="6" y2="6"/><line x1="3" x2="3.01" y1="12" y2="12"/><line x1="3" x2="3.01" y1="18" y2="18"/></svg>
             </button>
-            <button @click="toggleOrderedList()" :class="{ 'bg-gray-200 rounded': isActive('orderedList', updatedAt) }" class="p-1">
+            <button type="button" @click.prevent="toggleOrderedList()" :class="{ 'bg-gray-200 rounded': isActive('orderedList', updatedAt) }" class="p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-list-ordered"><line x1="10" x2="21" y1="6" y2="6"/><line x1="10" x2="21" y1="12" y2="12"/><line x1="10" x2="21" y1="18" y2="18"/><path d="M4 6h1v4"/><path d="M4 10h2"/><path d="M6 18H4c0-1 2-2 2-3s-1-1.5-2-1"/></svg>
             </button>
-            <button @click="addImage()" class="p-1">
+            <button type="button" @click="addImage()" class="p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-image"><rect width="18" height="18" x="3" y="3" rx="2" ry="2"/><circle cx="9" cy="9" r="2"/><path d="m21 15-3.086-3.086a2 2 0 0 0-2.828 0L6 21"/></svg>
             </button>
         </div>
